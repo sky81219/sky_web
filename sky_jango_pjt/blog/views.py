09ff331d9fc81ee6672cd2017fd9f9b9836745e6
sky <sky81219>
2021-02-24 07:14:18 +0900

    CBV_WEB_MAKE!

--- sky_jango_pjt/blog/views.py
+++ sky_jango_pjt/blog/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 # Create your views here.
 
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+
+"""
 def index(request):
     posts = Post.objects.all().order_by("-pk")
 
     return render(
         request,
-        'blog/index.html',
+        'blog/post_list.html',
         {
             'posts': posts,
         }
     )
-
+"""
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
 
